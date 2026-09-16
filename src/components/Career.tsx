d07f7055ede164c0a2cbe04d3623342d5e770a17
rexyyyyfx <rexyyfx@gmail.com>
2026-09-16 18:26:23 +0530

    Career.tsx

--- src/components/Career.tsx
+++ src/components/Career.tsx
@@ -5,8 +5,8 @@ const Career = () => {
     <div className="career-section section-container">
       <div className="career-container">
         <h2>
-          My career <span>&</span>
-          <br /> experience
+          Full Stack Developer <span>&</span>
+          <br /> 4 Year
         </h2>
         <div className="career-info">
           <div className="career-timeline">
